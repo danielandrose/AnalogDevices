8cf270c699d6d6d0e40a1c64f8276d64298aee65
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2024-10-24 18:43:06 +0530

    changes made

--- vite-project/src/components/Monitor.jsx
+++ vite-project/src/components/Monitor.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import '../cssFiles/Monitor.css'
 
-function Monitor(){
+function Monitor() {
     return (
-        <div>
-            <div>
+        <div className='main-monitor'>
+            <div className="driver-awake">
+                <h1>Is Driver is awake</h1>
+                <h2>Yes</h2>
+            </div>
+            <div className="sub-monitor">
                 <div>
-                    <h1>Is Driver is awake</h1>
-                    <h2>Yes</h2>
+                    <h1>Battery percentage</h1>
+                    <h3>80%</h3>
                 </div>
                 <div>
-                    <div>
-                        <h1>Battery percentage</h1>
-                        <h3>87%</h3>
-                    </div>
-                    <div>
-                        <h1>Air Pressure</h1>
-                        <h3>87%</h3>
-                        <h3>Safe</h3>
-                    </div>
+                    <h1>Air Pressure</h1>
+                    <h3>87%    <span>Safe</span></h3>
                 </div>
             </div>
         </div>
