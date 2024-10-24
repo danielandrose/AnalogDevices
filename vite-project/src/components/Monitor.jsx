7ec6684d56dbd9c48ad020318605c40760e5f8d6
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2024-10-24 14:13:45 +0530

    changes made

--- vite-project/src/components/Monitor.jsx
+++ vite-project/src/components/Monitor.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import '../cssFiles/Monitor.css'
+
 function Monitor(){
     return (
         <div>
-
+            <div>
+                <div>
+                    <h1>Is Driver is awake</h1>
+                    <h2>Yes</h2>
+                </div>
+                <div>
+                    <div>
+                        <h1>Battery percentage</h1>
+                        <h3>87%</h3>
+                    </div>
+                    <div>
+                        <h1>Air Pressure</h1>
+                        <h3>87%</h3>
+                        <h3>Safe</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
