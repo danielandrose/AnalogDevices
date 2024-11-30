1e41ba3d33b34b93e274e2a55136d2103a75cf08
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2024-11-30 23:27:30 +0530

    today's final changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import Header from "./components/Header"
 import NavBar from "./components/NavBar"
 import Monitor from "./components/Monitor"
 import { Footer } from './components/Footer'
+import TyrePressureDetails from './components/TyrePressureDetails'
+import BatteryDetails from './components/TyrePressureDetails'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      <NavBar />
-      <Header />
-      <Monitor />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+            <NavBar />
+            <Header />
+            <Monitor />
+            </>
+          }/>
+          <Route path="/tyre-pressure-details" element={<TyrePressureDetails/>} />
+          <Route path="/battery-details" element={<BatteryDetails/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/BatteryDetails.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function BatteryDetails() {
+    return <h1>Tyre Pressure</h1>
+}
